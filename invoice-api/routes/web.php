c92846e42e9b4180f8469402d4be58fbd974f55d
ello-lithuania <63595651+ello-lithuania@users.noreply.github.com>
2026-01-25 20:06:11 +0200

    Add deploy workflow

--- invoice-api/routes/web.php
+++ invoice-api/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/clear-cache', function() {
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('route:clear');
+    return 'Cache cleared!';
 });
