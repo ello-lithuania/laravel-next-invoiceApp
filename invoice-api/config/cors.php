a472ee832f1358ef96e5e5a4408fca6e9cd017af
ello-lithuania <63595651+ello-lithuania@users.noreply.github.com>
2026-01-25 17:45:46 +0200

    Add deploy workflow

--- invoice-api/config/cors.php
+++ invoice-api/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'https://mediumspringgreen-aardvark-872343.hostingersite.com'],
+    'allowed_origins' => ['http://localhost:3000', 'https://plum-viper-529383.hostingersite.com','https://mediumspringgreen-aardvark-872343.hostingersite.com'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
