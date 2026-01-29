5644ab95de2e6aaddf2663b44f4813142e512f67
ello-lithuania <63595651+ello-lithuania@users.noreply.github.com>
2026-01-29 11:54:50 +0200

    bug fixes, feature add

--- invoice-api/config/cors.php
+++ invoice-api/config/cors.php
@@ -2,11 +2,11 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     'allowed_origins' => ['http://localhost:3000', 'https://plum-viper-529383.hostingersite.com','https://mediumspringgreen-aardvark-872343.hostingersite.com'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
-    'max_age' => 0,
+    'max_age' => 86400,
     'supports_credentials' => true,
 ];
