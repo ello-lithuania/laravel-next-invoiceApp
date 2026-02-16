cafac117088b67867c868e49ad571088773a6fef
ello-lithuania <63595651+ello-lithuania@users.noreply.github.com>
2026-02-16 15:11:34 +0200

    few features, design update, fixes

--- invoice-frontend/app/(dashboard)/layout.tsx
+++ invoice-frontend/app/(dashboard)/layout.tsx
@@ -29,12 +29,14 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
       />
 
       <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-        <Header
-          sidebarOpen={sidebarOpen}
-          setSidebarOpen={setSidebarOpen}
-          sidebarExpanded={sidebarExpanded}
-          setSidebarExpanded={setSidebarExpanded}
-        />
+        <Suspense fallback={null}>
+          <Header
+            sidebarOpen={sidebarOpen}
+            setSidebarOpen={setSidebarOpen}
+            sidebarExpanded={sidebarExpanded}
+            setSidebarExpanded={setSidebarExpanded}
+          />
+        </Suspense>
 
         <main className="grow">
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
